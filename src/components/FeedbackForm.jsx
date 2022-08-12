6c92aff8bdb41c9d48be04e5ec32851a1d274626
Travis Brown <85965795+tychoterra@users.noreply.github.com>
2022-08-12 15:27:59 -0400

    created a rating select component which allows the user to select a rating from 1-10.

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,11 +1,26 @@
 import { useState } from "react";
 import Card from "./shared/Card";
 import Button from "./shared/Button";
+import RatingSelect from "./RatingSelect";
 
 function FeedbackForm() {
   const [text, setText] = useState("");
+  const [rating, setRating] = useState(10);
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
 
   const handleTextChange = (e) => {
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length <= 8) {
+      setMessage("Text must be 10 characters or more.");
+      setBtnDisabled(true);
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
+
     setText(e.target.value);
   };
 
@@ -13,7 +28,7 @@ function FeedbackForm() {
     <Card>
       <form>
         <h2>How would you rate your service with us?</h2>
-        {/* @todo - rating select component*/}
+        <RatingSelect select={(rating) => setRating(rating)} />
         <div className="input-group">
           <input
             onChange={handleTextChange}
@@ -21,8 +36,12 @@ function FeedbackForm() {
             placeholder="Write a review"
             value={text}
           />
-          <Button type="submit">Send</Button>
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
+
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -2,10 +2,11 @@
 import PropTypes from "prop-types";
 
 function Button({ children, version, type, isDisabled }) {
-  return;
-  <button type={type} disabled={isDisabled} className={`btn btn-${version}`}>
-    {children}
-  </button>;
+  return (
+    <button type={type} disabled={isDisabled} className={`btn btn-${version}`}>
+      {children}
+    </button>
+  );
 }
 
 Button.defaultProps = {
